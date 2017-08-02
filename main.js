789c1479538a2b52dc79d4030e060305238fcaa9
Robin Butler <Robin@Robins-MacBook-Air.local>
2017-08-02 16:21:15 -0400

    assignment

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log(customers);
+
+console.log(customers.results[0].name.first);
+console.log(customers.results[1].name.first);
+console.log(customers.results[2].name.first);
+
+for (i = 0; i < customers.results.length; i++){
+  results.persons[].name = customers[].name ("first,last")
+  results.persons[].email= customers[].email
+  results.persons[].location= customers[].location ("street,city,state,postcode")
+  results.persons[].phone=customers[].phone
+
+document.querySelector('.container').innerHTML = persons
+// "name":{"title":"mr","first":"curtis","last":"ryan"},
+// "location":{"street":"8073 rolling green rd","city":"south valley","state":"missouri","postcode":99356},
+// "email":"curtis.ryan@example.com"
